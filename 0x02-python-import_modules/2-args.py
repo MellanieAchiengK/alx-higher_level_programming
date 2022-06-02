396331b2f5bbbe388471682e915ec1871be5d680
mellanieachieng <mellanie.achieng@gmail.com>
2022-06-02 17:25:18 +0000

    pycodestyle fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-def print_arg(argv):
-    n = len(argv) - 1
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv[1:]
+    n = len(argv)
     if n == 0:
         print("{:d} arguments.".format(n))
-        return
     else:
         if n == 1:
             print("{:d} argument:".format(n))
@@ -11,8 +12,5 @@ def print_arg(argv):
             print("{:d} arguments:".format(n))
         i = 1
         while i <= n:
-            print("{:d}: {:s}".format(i, argv[i]))
+            print("{:d}: {:s}".format(i, sys.argv[i]))
             i += 1
-if __name__ == "__main__":
-    import sys
-    print_arg(sys.argv)
