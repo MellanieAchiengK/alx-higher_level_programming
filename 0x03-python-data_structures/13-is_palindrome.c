77844b6752f6b5fb3c7aa97f16cd48f611c9a402
mellanieachieng <mellanie.achieng@gmail.com>
2022-06-06 17:09:23 +0000

    13-is_palidrome fix

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,23 +1,41 @@
 #include "lists.h"
-#include <stddef.h>
+
 /**
- * is_palindrome - checks if palindrome
- * @head: head of node
- * Return: 0 if not, 1 if is
+ * is_palindrome - determine if singly linked list is palindrome
+ * @head: pointer to head of singly linked list
+ * Return: 0 if not, 1 if palindrome
  */
 int is_palindrome(listint_t **head)
 {
-	unsigned int len = 1;
-	listint_t *temp;
+  listint_t *tmp = *head;
+  unsigned int size = 0, i = 0;
+  int data[10240];
 
-	if (head == NULL|| *head == NULL)
-		return (1);
+  if (head == NULL) /* non-existing list is not */
+    return (0);
 
-	temp = *head;
-	while (temp) /* get len of list */
-	{
-		temp = temp->next;
-		len++;
-	}
+  if (*head == NULL) /* empty list is palindrome */
+    return (1);
+
+  while (tmp) /* find size of linked list */
+    {
+      tmp = tmp->next;
+      size += 1;
+    }
+  if (size == 1) /* single node list is palindrome */
+    return (1);
+
+  tmp = *head;
+  while (tmp) /* pull node data into array to compare */
+    {
+      data[i++] = tmp->n;
+      tmp = tmp->next;
+    }
+
+  for (i = 0; i <= (size/2); i++)
+    {
+      if (data[i] != data[size - i - 1])
 	return (0);
+    }
+  return (1);
 }
