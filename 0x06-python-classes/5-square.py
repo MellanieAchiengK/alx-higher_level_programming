7517e9e48befc79f4ec463aed8de3fbf4e4f6436
mellanieachieng <mellanie.achieng@gmail.com>
2022-06-21 17:28:01 +0000

    pycodestyle & documentation fix

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -5,6 +5,7 @@ Define class Square
 
 
 class Square:
+    """ defines private attribute """
     def __init__(self, size=0):
         self.__size = size
 
--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,7 +4,7 @@ Defines class Square """
 
 
 class Square:
-   """ defines private attribute """
+    """ defines private attribute """
     def __init__(self, size=0, position=(0, 0)):
         self.__size = size
         self.__position = position
