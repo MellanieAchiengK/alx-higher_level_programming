0e71def31ea5118529f3ae16fbcffe274819bc97
mellanieachieng <mellanie.achieng@gmail.com>
2022-08-09 22:02:05 +0000

    Advance Tasks bug fix

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,2 +1,2 @@
 -- Displays max temperature
-SELECT state, MAX(value) AS max_temp FROM temperatures ORDER BY state;
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state;
