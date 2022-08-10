38e0796cec66423999c3d9b202e3911caccf2673
mellanieachieng <mellanie.achieng@gmail.com>
2022-08-10 21:54:29 +0000

    bug fix

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,7 @@
 -- lists all shows contained in hbtn_0d_tvshows 
 -- have at least one genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows as tv_shows
-	INNER JOIN tv_show_genres as tv_show_genres 
-	    ON tv_shows.id = tv_show_genres.genre_id
+  FROM tv_shows AS tv_shows
+	 INNER JOIN tv_show_genres AS tv_show_genres
+	     ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,7 +1,7 @@
 -- lists all shows contained in hbtn_0d_tvshows 
 -- return NULL if no genre
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows as tv_shows
-	LEFT JOIN tv_show_genres as tv_show_genres 
-	    ON tv_shows.id = tv_show_genres.genre_id
+  FROM tv_shows AS tv_shows
+	 LEFT JOIN tv_show_genres AS tv_show_genres
+	    ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
